28c6c69684d72b33e7b2caa4b78e32515d8fac4e
Danil Goncharov <65015830+bigwitch3r@users.noreply.github.com>
2021-06-09 23:36:48 +0300

    Лабораторная работа №6
    Загрузил проект лабораторной работы №6

---
+++ DBUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApp2
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "127.0.0.1";
+            int port = 3306;
+            string database = "mydb";
+            string username = "root";
+            string password = "TogetherForever27!";
+
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+
+    }
+}
